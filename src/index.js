e656bb30386622a6cfbd07f4562a5f0e8355b277
lijia <lijia@yunphant.com>
2024-11-03 11:32:57 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.querySelector("#root"));
+
+const categories = [
+  { id: 1, name: "推荐" },
+  { id: 2, name: "西餐" },
+  { id: 3, name: "中餐" },
+  { id: 4, name: "海鲜" },
+];
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <ur>
+      {categories.map((item) => {
+        return <li key={item.id}>{item.name}</li>;
+      })}
+    </ur>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
